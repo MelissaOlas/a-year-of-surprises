43ea051377ca577bb496d667c444083c3becf1f1
Melissa Olas <melissa.olas@gmail.com>
2025-11-15 13:51:49 +0100

    adding camera style (in the work)

--- src/components/02-apr/camera/PhotoCapture.jsx
+++ src/components/02-apr/camera/PhotoCapture.jsx
@@ -11,7 +11,7 @@ const PhotoCapture = () => {
         <span className="material-symbols-outlined">door_open</span>
       </button> */}
       <div className="camera">
-        <div className="bar">
+        <div className="top">
           <div className="button-1"></div>
           <div className="button-2"></div>
           <div className="button-3">
@@ -19,23 +19,23 @@ const PhotoCapture = () => {
           </div>
           <div className="button-4"></div>
         </div>
+
         <div className="camera-body">
-          <div className="camera-lens"></div>
-        </div>
-        <div className="camera-body">
-          <div className="top">
+          <div className="bar">
             <div className="small-glass"></div>
             <div className="flash">
               <div className="box-1"></div>
               <div className="box-2"></div>
             </div>
           </div>
+
           <div className="center">
             <div className="dot"></div>
             <div className="lens">
               <div className="inner-lens"></div>
             </div>
           </div>
+
           <div className="bottom"></div>
         </div>
       </div>
