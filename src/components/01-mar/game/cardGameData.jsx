45b70cee3d079074221cc96f57c2c2b10618a9e9
Melissa Olas <melissa.olas@gmail.com>
2025-11-17 16:31:18 +0100

    adding supabase and storing mars assets

--- src/components/01-mar/game/cardGameData.jsx
+++ src/components/01-mar/game/cardGameData.jsx
@@ -1,50 +1,59 @@
+import { supabase } from "../../../api/supabaseClient";
+
+function getImageUrl(filename) {
+  const { data } = supabase.storage
+    .from("images")
+    .getPublicUrl(`mars/${filename}`);
+  return data.publicUrl;
+}
+
 const baseCards = [
   {
     id: "1",
     name: "photo1",
-    img: "/assets/cardGame/first.jpg",
+    img: getImageUrl("first.jpg"),
     matched: false,
   },
   {
     id: "2",
     name: "photo2",
-    img: "/assets/cardGame/second.jpg",
+    img: getImageUrl("second.jpg"),
     matched: false,
   },
   {
     id: "3",
     name: "photo3",
-    img: "/assets/cardGame/third.jpg",
+    img: getImageUrl("third.jpg"),
     matched: false,
   },
   {
     id: "4",
     name: "photo4",
-    img: "/assets/cardGame/fourth.jpg",
+    img: getImageUrl("fourth.jpg"),
     matched: false,
   },
   {
     id: "5",
     name: "photo5",
-    img: "/assets/cardGame/fifth.jpg",
+    img: getImageUrl("fifth.jpg"),
     matched: false,
   },
   {
     id: "6",
     name: "photo6",
-    img: "/assets/cardGame/sixth.jpg",
+    img: getImageUrl("sixth.jpg"),
     matched: false,
   },
   {
     id: "7",
     name: "photo7",
-    img: "/assets/cardGame/seventh.jpg",
+    img: getImageUrl("seventh.jpg"),
     matched: false,
   },
   {
     id: "8",
     name: "photo8",
-    img: "/assets/cardGame/eighth.jpeg",
+    img: getImageUrl("eighth.jpeg"),
     matched: false,
   },
 ];
@@ -58,3 +67,64 @@ const cardGameData = [
 ];
 
 export default cardGameData;
+
+// const baseCards = [
+//   {
+//     id: "1",
+//     name: "photo1",
+//     img: "/assets/cardGame/first.jpg",
+//     matched: false,
+//   },
+//   {
+//     id: "2",
+//     name: "photo2",
+//     img: "/assets/cardGame/second.jpg",
+//     matched: false,
+//   },
+//   {
+//     id: "3",
+//     name: "photo3",
+//     img: "/assets/cardGame/third.jpg",
+//     matched: false,
+//   },
+//   {
+//     id: "4",
+//     name: "photo4",
+//     img: "/assets/cardGame/fourth.jpg",
+//     matched: false,
+//   },
+//   {
+//     id: "5",
+//     name: "photo5",
+//     img: "/assets/cardGame/fifth.jpg",
+//     matched: false,
+//   },
+//   {
+//     id: "6",
+//     name: "photo6",
+//     img: "/assets/cardGame/sixth.jpg",
+//     matched: false,
+//   },
+//   {
+//     id: "7",
+//     name: "photo7",
+//     img: "/assets/cardGame/seventh.jpg",
+//     matched: false,
+//   },
+//   {
+//     id: "8",
+//     name: "photo8",
+//     img: "/assets/cardGame/eighth.jpeg",
+//     matched: false,
+//   },
+// ];
+
+// const cardGameData = [
+//   ...baseCards,
+//   ...baseCards.map((card) => ({
+//     ...card,
+//     id: card.id + "-duplicate",
+//   })),
+// ];
+
+// export default cardGameData;
